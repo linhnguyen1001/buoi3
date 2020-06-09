3efac3f42ce8418bf2285433fdc06e2977e0848a
Administrator <Administrator@PC219>
2020-06-09 08:39:07 +0700

    tuan 5

---
+++ buoi3lab456/Controllers/CoureseController.cs
@@ -0,0 +1,30 @@
+﻿using buoi3lab456.Models;
+using buoi3lab456.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace buoi3lab456.Controllers
+{
+    public class CoureseController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoureseController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+        // GET: Courese
+}
---
+++ buoi3lab456/Controllers/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using buoi3lab456.Models;
+
+namespace buoi3lab456.Controllers
+{
+    internal class CourseViewModel
+    {
+        public List<Category> Categories { get; set; }
+    }
+}
